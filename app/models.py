f33c320e1d38b3cac993cb5f7ec98cf3cbf35f60
geer26 <gergo.kurucz@gmail.com>
2022-03-18 06:59:34 +0100

    v0.1

--- app/models.py
+++ app/models.py
@@ -10,6 +10,12 @@ def user_identity_lookup(user):
     return user.id
 
 
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(id=identity).one_or_none()
+
+
 class User(db.Model):
     id = db.Column(db.Integer, index=True, primary_key=True)
     username = db.Column(db.String(32), unique=True, nullable=False)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, current_user, create_access_token
 
 from app import app, jwt
-from flask import request, redirect, Response
+from flask import request, redirect, jsonify, make_response
+
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -15,13 +17,26 @@ def protected():
     return 'protected'
 
 
+@app.route("/who_am_i", methods=["GET"])
+@jwt_required()
+def whoami():
+    # We can now access our sqlalchemy User object via `current_user`.
+    return jsonify(
+        id=current_user.id,
+        full_name=current_user.full_name,
+        username=current_user.username,
+    )
+
+
 @app.route('/login')
 def login():
     auth = request.authorization
-    if auth and auth.password == 'password':
-        return redirect('/protected')
-    #return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
-    resp = Response()
-    resp.data = '<h1>LOGINPAGE</h1>'
-    resp.headers['WWW-Authenticate'] = 'Basic'
-    return resp, 401
+    if auth:
+        username = auth.username
+        password = auth.password
+        user = User.query.filter_by(username=username).one_or_none()
+        if not user or not user.check_password(password):
+            return jsonify("Wrong username or password"), 401
+        access_token = create_access_token(identity=user)
+        return jsonify(access_token=access_token)
+    return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
